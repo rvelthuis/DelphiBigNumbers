5ee65661313d4be40dd061d96460c6de68b50e6e
rvelthuis <velthuis@gmail.com>
2017-12-25 02:36:13 +0100

    Removed some unnecessary lines

--- Source/Velthuis.StrConsts.pas
+++ Source/Velthuis.StrConsts.pas
@@ -3,32 +3,24 @@ unit Velthuis.StrConsts;
 interface
 
 resourcestring
-  // BigIntegers
-  SErrorParsingFmt           = '''%s'' is not a valid %s value';
-  SDivisionByZero            = 'Division by zero';
-  SOverflow                  = 'Resulting value too big to represent';
-  SOverflowFmt               = '%s: Resulting value too big to represent';
-  SInvalidOperation          = 'Invalid operation';
-  SConversionFailedFmt       = '%s value too large for conversion to %s';
-  SInvalidArgumentFloatFmt   = '%s parameter may not be NaN or +/- Infinity';
-  SInvalidArgumentBase       = 'Base parameter must be in the range 2..36';
-  SInvalidArgumentFmt        = 'Invalid argument: %s';
-  SSqrtBigInteger            = 'Negative values not allowed for Sqrt';
-  SNoInverse                 = 'No modular inverse possible';
-  SNegativeExponent          = 'Negative exponent %s not allowed';
-
-  // BigDecimals
+  SErrorParsingFmt         = '''%s'' is not a valid %s value';
+  SDivisionByZero          = 'Division by zero';
+  SOverflow                = 'Resulting value too big to represent';
+  SOverflowFmt             = '%s: Resulting value too big to represent';
+  SInvalidOperation        = 'Invalid operation';
+  SConversionFailedFmt     = '%s value too large for conversion to %s';
+  SInvalidArgumentFloatFmt = '%s parameter may not be NaN or +/- Infinity';
+  SInvalidArgumentBase     = 'Base parameter must be in the range 2..36';
+  SInvalidArgumentFmt      = 'Invalid argument: %s';
+  SSqrtBigInteger          = 'Negative values not allowed for Sqrt';
+  SNoInverse               = 'No modular inverse possible';
+  SNegativeExponent        = 'Negative exponent %s not allowed';
   SUnderflow               = 'Resulting value too small to represent';
   SRounding                = 'Rounding necessary';
   SExponent                = 'Exponent to IntPower outside the allowed range';
-
-  // BigRationals
-//  SDivisionByZero       = 'Division by zero';
-//  SInvalidArgumentFmt     = 'Invalid argument: %s';
-  SNoInfinity      = 'Infinity cannot be converted to BigRational';
-  SNoNan           = 'NaN cannot be converted to BigRational';
-  SZeroDenominator = 'BigRational denominator cannot be zero';
-//  SErrorParsingFmt      = '''%s'' is not a valid BigRational value';
+  SNoInfinity              = 'Infinity cannot be converted to BigRational';
+  SNoNan                   = 'NaN cannot be converted to BigRational';
+  SZeroDenominator         = 'BigRational denominator cannot be zero';
 
 implementation
 
